649ab61d5a9d3d07f5a765f0f04c328b52ab3182
alex <emoguy@mail.ru>
2015-07-22 17:15:04 +0300

    Add migration

--- config/database.php
+++ config/database.php
@@ -52,17 +52,29 @@ return [
             'prefix'   => '',
         ],
 
-        'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
-        ],
+        // 'mysql' => [
+        //     'driver'    => 'mysql',
+        //     'host'      => env('DB_HOST', 'localhost'),
+        //     'database'  => env('DB_DATABASE', 'laravel5'),
+        //     'username'  => env('DB_USERNAME', 'root'),
+        //     'password'  => env('DB_PASSWORD', ''),
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        //     'strict'    => false,
+        // ],
+
+       'mysql' => array(
+                'driver'    => 'mysql',
+                'host'      => '127.0.0.1',
+                'database'  => 'laravel_db',
+                'username'  => 'root',
+                'password'  => '',
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+                'strict'    => false,
+        ),
 
         'pgsql' => [
             'driver'   => 'pgsql',
